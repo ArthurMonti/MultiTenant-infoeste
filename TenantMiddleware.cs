0fd20fa1ab150c0f6b75afa4d5d181abf894e68c
Arthur Monti <arthur_monti@hotmail.com>
2025-10-30 16:30:15 -0300

    final

--- TenantMiddleware.cs
+++ TenantMiddleware.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using CursoInfoeste.Models;
+using System.Diagnostics;
 
 namespace CursoInfoeste
 {
@@ -13,7 +14,17 @@ namespace CursoInfoeste
 
         public async Task InvokeAsync(HttpContext context, Persistencia persistencia)
         {
-            var tenantId = context.Request.Headers["TenantId"].ToString();
+            string tenantId = string.Empty;
+            if (context.User != null && context.User.Identity.IsAuthenticated)
+            {
+                var claim = context.User.Claims?.FirstOrDefault(x => x.Type == "TenantId");
+                tenantId = claim.Value.ToString();
+
+            }else
+            {
+                tenantId = context.Request.Headers["TenantId"].ToString();
+                
+            }
             persistencia.TenantId = string.IsNullOrEmpty(tenantId) ? 0 : int.Parse(tenantId);
             //Antes do endpoint
             await _next(context);
